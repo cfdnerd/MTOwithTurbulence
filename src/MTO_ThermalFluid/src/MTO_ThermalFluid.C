c4b7f384ecd184da74a820f5b5e1751a9c6275d3
cfdnerd <iammathewknight@gmail.com>
2026-02-20 22:04:58 +0530

    Update MTO_ThermalFluid.C

--- src/MTO_ThermalFluid/src/MTO_ThermalFluid.C
+++ src/MTO_ThermalFluid/src/MTO_ThermalFluid.C
@@ -32,11 +32,17 @@ int main(int argc, char *argv[])
     MTOdiag::setConfig(diagLevel, diagEvery, diagRank, diagOptStart);
     while (simple.loop(runTime))
     {
+        MTOdiag::logMetric(runTime, opt, "Heartbeat", "loop_enter", 1.0);
         #include "update.H"
+        MTOdiag::logMetric(runTime, opt, "Heartbeat", "after_update", 1.0);
         turbulence->correct();
+        MTOdiag::logMetric(runTime, opt, "Heartbeat", "after_turbulence_correct", 1.0);
         #include "Primal_U.H"
+        MTOdiag::logMetric(runTime, opt, "Heartbeat", "after_primal_U", 1.0);
         #include "Primal_T.H"
+        MTOdiag::logMetric(runTime, opt, "Heartbeat", "after_primal_T", 1.0);
         #include "AdjointHeat_Tb.H"
+        MTOdiag::logMetric(runTime, opt, "Heartbeat", "after_adjoint_Tb", 1.0);
         const bool doTurbAdjSolveThisOpt =
         (
             adjTurbLagEvery <= 1
@@ -46,16 +52,23 @@ int main(int argc, char *argv[])
         MTOdiag::logMetric(runTime, opt, "AdjointLoop", "doTurbAdjSolve", doTurbAdjSolveThisOpt ? 1.0 : 0.0);
         for (label pass = 0; pass < nAdjTurbPasses; pass++)
         {
+            MTOdiag::logMetric(runTime, opt, "Heartbeat", word("adj_pass_") + Foam::name(pass) + "_enter", 1.0);
             if (doTurbAdjSolveThisOpt || pass == 0)
             {
                 #include "Adjoint_kOmegaSST.H"
+                MTOdiag::logMetric(runTime, opt, "Heartbeat", word("adj_pass_") + Foam::name(pass) + "_after_kOmega", 1.0);
             }
             #include "AdjointHeat_Ub.H"
+            MTOdiag::logMetric(runTime, opt, "Heartbeat", word("adj_pass_") + Foam::name(pass) + "_after_Ub", 1.0);
             #include "AdjointFlow_Ua.H"
+            MTOdiag::logMetric(runTime, opt, "Heartbeat", word("adj_pass_") + Foam::name(pass) + "_after_Ua", 1.0);
         }
         #include "costfunction.H"              
+        MTOdiag::logMetric(runTime, opt, "Heartbeat", "after_costfunction", 1.0);
         #include "sensitivity.H"
+        MTOdiag::logMetric(runTime, opt, "Heartbeat", "after_sensitivity", 1.0);
         #include "writeSolverStatus.H"
+        MTOdiag::logMetric(runTime, opt, "Heartbeat", "after_writeSolverStatus", 1.0);
     }
     #include "finalize.H"
     return 0;
